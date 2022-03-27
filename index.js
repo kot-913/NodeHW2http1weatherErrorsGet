05bb51b9ab3b24bc3852e5a7bfa68e80f64679f8
Ecaterina Pogorelova <kot91319@gmail.com>
2022-03-27 12:34:04 +0300

    something wrong here

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 const http = require('http');
 
-
- 
 const server = http.createServer((req, res) => {
-    if(req.url === 'localhost:3004/weather' && req.method === 'GET') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.write(JSON.stringify(data));
-        res.end(); 
-    } else {
-        res.writeHead(404, {'Content-Type': 'application/json'}); 
-        res.end(`{"error": "${http.STATUS_CODES[404]}"}`);
-    }
+
+    const http = require('http');
+
+    const url = 'http://localhost:3004/weather';
+
+    let data = '';
   
+    const request = http.request(url, (response) => {
+        
+        response.on('data', (chunk) => {
+            data = data + chunk.toString();
+        });
+      
+        response.on('end', () => {
+            const body = JSON.parse(data);
+
+            
+                        switch (body.status) {
+                            case 404:
+                                res.end(`${http.STATUS_CODES[404]}`);
+                                break;
+                            case 400:
+                                res.end(`${http.STATUS_CODES[400]}`);
+                                break;
+                            case 401:
+                                res.end( `${http.STATUS_CODES[401]}`);
+                                break;
+                            case 403:
+                                res.end(`${http.STATUS_CODES[403]}`);
+                                break;
+                            case 500:
+                                res.end(`${http.STATUS_CODES[500]}`);
+                                break;
+                            default:
+                                res.end(`<h1>${JSON.stringify(body)}</h1>`);;
+                                break;
+                        }
+        });
+    })
+    request.end();
+ 
 });
+
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
